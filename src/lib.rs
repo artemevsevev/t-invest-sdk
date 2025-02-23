a1520e7f38738691613c23df7ede140694891b03
Artem Evsevev <artem@qrsoft.ru>
2025-02-23 11:13:48 +0300

    feat: add decimal converters

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ use api::{
     sandbox_service_client::SandboxServiceClient, signal_service_client::SignalServiceClient,
     stop_orders_service_client::StopOrdersServiceClient, users_service_client::UsersServiceClient,
 };
+use api::{MoneyValue, Quotation};
+use rust_decimal::prelude::ToPrimitive;
+use rust_decimal::Decimal;
 use thiserror::Error;
 use tonic::transport::ClientTlsConfig;
 use tonic::{
@@ -188,5 +191,242 @@ impl TInvestSdk {
 
     pub fn users(&self) -> UsersServiceClient<InterceptedService<Channel, TInvestInterceptor>> {
         self.users_service_client.clone()
+    }
+}
+
+impl From<Quotation> for Decimal {
+    fn from(quotation: Quotation) -> Self {
+        Decimal::new(quotation.units, 0) + Decimal::new(quotation.nano as i64, 9).normalize()
+    }
+}
+
+impl From<MoneyValue> for Decimal {
+    fn from(money_value: MoneyValue) -> Self {
+        Decimal::new(money_value.units, 0) + Decimal::new(money_value.nano as i64, 9).normalize()
+    }
+}
+
+impl TryFrom<Decimal> for Quotation {
+    type Error = String;
+
+    fn try_from(value: Decimal) -> Result<Self, Self::Error> {
+        let units = match value.trunc().to_i64() {
+            Some(v) => v,
+            None => {
+                return Err(format!("Can't convert decimal {} to quotation", value));
+            }
+        };
+
+        let nano = match (value.fract() * Decimal::new(1_000_000_000, 0)).to_i32() {
+            Some(v) => v,
+            None => {
+                return Err(format!("Can't convert decimal {} to quotation", value));
+            }
+        };
+
+        Ok(Quotation { units, nano })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rust_decimal_macros::dec;
+
+    use super::*;
+
+    #[test]
+    fn quotation_to_decimal() {
+        assert_eq!(dec!(0), Quotation { units: 0, nano: 0 }.into());
+
+        assert_eq!(
+            dec!(100),
+            Quotation {
+                units: 100,
+                nano: 0
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(-100),
+            Quotation {
+                units: -100,
+                nano: 0
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(114.25),
+            Quotation {
+                units: 114,
+                nano: 250000000
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(-200.20),
+            Quotation {
+                units: -200,
+                nano: -200000000
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(-0.01),
+            Quotation {
+                units: -0,
+                nano: -10000000
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(999.999999999),
+            Quotation {
+                units: 999,
+                nano: 999999999
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(-999.999999999),
+            Quotation {
+                units: -999,
+                nano: -999999999
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn money_value_to_decimal() {
+        assert_eq!(
+            dec!(0),
+            MoneyValue {
+                units: 0,
+                nano: 0,
+                currency: "".to_string()
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(100),
+            MoneyValue {
+                units: 100,
+                nano: 0,
+                currency: "".to_string()
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(-100),
+            MoneyValue {
+                units: -100,
+                nano: 0,
+                currency: "".to_string()
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(114.25),
+            MoneyValue {
+                units: 114,
+                nano: 250000000,
+                currency: "".to_string()
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(-200.20),
+            MoneyValue {
+                units: -200,
+                nano: -200000000,
+                currency: "".to_string()
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(-0.01),
+            MoneyValue {
+                units: -0,
+                nano: -10000000,
+                currency: "".to_string()
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(999.999999999),
+            MoneyValue {
+                units: 999,
+                nano: 999999999,
+                currency: "".to_string()
+            }
+            .into()
+        );
+
+        assert_eq!(
+            dec!(-999.999999999),
+            MoneyValue {
+                units: -999,
+                nano: -999999999,
+                currency: "".to_string()
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn decimal_to_quotation() {
+        assert_eq!(Ok(Quotation { units: 0, nano: 0 }), dec!(0).try_into());
+
+        assert_eq!(
+            Ok(Quotation {
+                units: 114,
+                nano: 250000000,
+            }),
+            dec!(114.25).try_into()
+        );
+
+        assert_eq!(
+            Ok(Quotation {
+                units: -200,
+                nano: -200000000,
+            }),
+            dec!(-200.20).try_into()
+        );
+
+        assert_eq!(
+            Ok(Quotation {
+                units: -0,
+                nano: -10000000,
+            }),
+            dec!(-0.01).try_into()
+        );
+
+        assert_eq!(
+            Ok(Quotation {
+                units: 999,
+                nano: 999999999,
+            }),
+            dec!(999.999999999).try_into()
+        );
+
+        assert_eq!(
+            Ok(Quotation {
+                units: -999,
+                nano: -999999999,
+            }),
+            dec!(-999.999999999).try_into()
+        );
     }
 }
